c113d1a2ad49a13b2161495676f9c7a2962b0567
Ankit H. Vishwakarma <ankitvi.786.av@gmail.com>
2019-09-12 18:02:44 +0530

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
   mongoURI:
-    "mongodb+srv://zhulie:zhulie@cause-code-dvs2x.mongodb.net/test?retryWrites=true&w=majority"
+    "mongodb+srv://<username>:<password>@cause-code-dvs2x.mongodb.net/test?retryWrites=true&w=majority"
 };
