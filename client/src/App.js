16942bd99ca8b48d05c166a33d32bc8431ac6307
Ankit <ankitvi.2818.av@gmail.com>
2019-09-22 14:43:11 +0530

    Added functionality to see all the profiles.

--- client/src/App.js
+++ client/src/App.js
@@ -20,6 +20,7 @@ import CreateProfile from "./components/create-profile/CreateProfile";
 import EditProfile from "./components/edit-profile/EditProfile";
 import AddExperience from "./components/add-credentials/AddExperience";
 import AddEducation from "./components/add-credentials/AddEducation";
+import Profiles from "./components/profiles/Profiles";
 
 import "./App.css";
 
@@ -58,6 +59,7 @@ function App() {
           <div className="container">
             <Route exact path="/register" component={Register} />
             <Route exact path="/login" component={Login} />
+            <Route exact path="/profiles" component={Profiles} />
             <Switch>
               <PrivateRoute exact path="/dashboard" component={Dashboard} />
             </Switch>
---
+++ client/src/components/profiles/Profiles.js
@@ -0,0 +1,59 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import Proptypes from "prop-types";
+import Spinner from "../common/Spinner";
+import { getProfiles } from "../../actions/profileActions";
+import ProfileItem from "./ProfileItem";
+
+class Profiles extends Component {
+  componentDidMount() {
+    this.props.getProfiles();
+  }
+
+  render() {
+    const { profiles, loading } = this.props.profile;
+    let profileItems;
+
+    if (profiles === null || loading) {
+      profileItems = <Spinner />;
+    } else {
+      if (profiles.length > 0) {
+        profileItems = profiles.map(profile => (
+          <ProfileItem key={profile._id} profile={profile} />
+        ));
+      } else {
+        profileItems = <h4>No Profiles Found.</h4>;
+      }
+    }
+
+    return (
+      <div className="profiles">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <h1 className="display-4 text-center">Developer Profiles</h1>
+              <p className="lead text-center">
+                Browse and Connect with developers
+              </p>
+              {profileItems}
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+Profiles.propTypes = {
+  getProfiles: Proptypes.func.isRequired,
+  profile: Proptypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  profile: state.profile
+});
+
+export default connect(
+  mapStateToProps,
+  { getProfiles }
+)(Profiles);
