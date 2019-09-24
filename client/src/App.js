6a53ada94ed661c53ab1baee948e229f36377613
Ankit <ankitvi.2818.av@gmail.com>
2019-09-24 21:39:09 +0530

    Created Profile of single user. Profile Part is completed.

--- client/src/App.js
+++ client/src/App.js
@@ -21,6 +21,8 @@ import EditProfile from "./components/edit-profile/EditProfile";
 import AddExperience from "./components/add-credentials/AddExperience";
 import AddEducation from "./components/add-credentials/AddEducation";
 import Profiles from "./components/profiles/Profiles";
+import Profile from "./components/profile/Profile";
+import NotFound from "./components/not-found/NotFound";
 
 import "./App.css";
 
@@ -60,6 +62,7 @@ function App() {
             <Route exact path="/register" component={Register} />
             <Route exact path="/login" component={Login} />
             <Route exact path="/profiles" component={Profiles} />
+            <Route exact path="/profile/:handle" component={Profile} />
             <Switch>
               <PrivateRoute exact path="/dashboard" component={Dashboard} />
             </Switch>
@@ -91,6 +94,7 @@ function App() {
                 component={AddEducation}
               />
             </Switch>
+            <Route exact path="/not-found" component={NotFound} />
           </div>
           <Footer />
         </div>
--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-  mongoURI: `mongodb+srv://zhulie:zhulie@cause-code-dvs2x.mongodb.net/test?retryWrites=true&w=majority`,
-  secretOrKey: "zhulie"
+  mongoURI: "Add Your Own",
+  secretOrKey: "Add Your Own"
 };
